c8594deb4f1ad26f2328adbf0c3032b1ca444c48
Akshay <akshay@istarindia.com>
2017-03-17 18:29:11 +0530

    commiting further changes

---
+++ app/src/main/java/pro/viksit/com/viksit/home/BatchCodeActivity.java
@@ -0,0 +1,15 @@
+package pro.viksit.com.viksit.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pro.viksit.com.viksit.R;
+
+public class BatchCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batch_code);
+    }
+}
