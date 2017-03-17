6f7892622ee92640f79fa33a9fe9ef61cd0e65cd
feroz-istar <feroz@istarindia.com>
2017-03-17 22:32:01 +0530

    Adding roles

--- app/src/main/java/pro/viksit/com/viksit/dashboard/activity/DashboardActivity.java
+++ app/src/main/java/pro/viksit/com/viksit/dashboard/activity/DashboardActivity.java
@@ -3,16 +3,19 @@ package pro.viksit.com.viksit.dashboard.activity;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import pro.viksit.com.viksit.R;
 import pro.viksit.com.viksit.dashboard.util.BottomBarUtil;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
         new BottomBarUtil().setupBottomBar(bottomNavigationView,DashboardActivity.this,R.id.task);
