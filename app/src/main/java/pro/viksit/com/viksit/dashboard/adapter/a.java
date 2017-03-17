855c7da622af2d6c582ee8bb098ce3e23bab33eb
Akshay <akshay@istarindia.com>
2017-03-17 12:31:05 +0530

    viksit folder structure

---
+++ app/src/main/java/pro/viksit/com/viksit/dashboard/adapter/a.java
@@ -0,0 +1,8 @@
+package pro.viksit.com.viksit.dashboard.adapter;
+
+/**
+ * Created by ravy on 17/03/2017.
+ */
+
+public class a {
+}
---
+++ app/src/main/java/pro/viksit/com/viksit/dashboard/util/a.java
@@ -0,0 +1,9 @@
+
+package pro.viksit.com.viksit.dashboard.util;
+
+/**
+ * Created by ravy on 17/03/2017.
+ */
+
+public class a {
+}
