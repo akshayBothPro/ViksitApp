025de98d3d46cd7b29ceffdb86b41718148d4eaf
feroz-istar <feroz@istarindia.com>
2017-03-18 18:45:13 +0530

    Adding roles

--- app/src/main/java/pro/viksit/com/viksit/dashboard/pojo/a.java
+++
@@ -1,8 +0,0 @@
-package pro.viksit.com.viksit.dashboard.pojo;
-
-/**
- * Created by ravy on 17/03/2017.
- */
-
-public class a {
-}
--- app/src/main/java/pro/viksit/com/viksit/dashboard/util/a.java
+++
@@ -1,9 +0,0 @@
-
-package pro.viksit.com.viksit.dashboard.util;
-
-/**
- * Created by ravy on 17/03/2017.
- */
-
-public class a {
-}
