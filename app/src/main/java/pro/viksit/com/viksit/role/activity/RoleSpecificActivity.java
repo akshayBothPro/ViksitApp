5fff54692c972bfb69ee1db4108e25b758bd0822
Akshay <akshay@istarindia.com>
2017-04-13 22:06:42 +0530

    Leader Board Ready for tablet

---
+++ app/src/main/java/pro/viksit/com/viksit/role/activity/RoleSpecificActivity.java
@@ -0,0 +1,15 @@
+package pro.viksit.com.viksit.role.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pro.viksit.com.viksit.R;
+
+public class RoleSpecificActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role_specific);
+    }
+}
